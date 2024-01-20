a30d883806ffcac79ddff35736976952473c950f
unknown <ayushcodingprofile@gmail.com>
2024-01-20 13:23:12 +0530

    setup node express with typescript

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,7 @@
-export {};
+import express from 'express';
+const app = express();
+//middlewares
+app.use(express.json());
+//connections
+app.listen(5000, () => console.log("Server Open"));
 //# sourceMappingURL=index.js.map
---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+
+
+//connections
+app.listen(5000, ()=> console.log("Server Open"));
