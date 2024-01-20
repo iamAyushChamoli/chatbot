63eeb992b15f6aa5bbbf33f60aff75ef14331154
unknown <ayushcodingprofile@gmail.com>
2024-01-20 15:34:59 +0530

    Get all users

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,4 +1,20 @@
-export const getAllUsers = () => {
-    //get all users
+import User from "../models/user.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: " SIGNUP OK", users });
+    }
+    catch (error) {
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,21 @@
-export const getAllUsers=()=>{
-    //get all users
+import User from "../models/user.js"
+import { Request, Response, NextFunction } from "express";
+
+export const getAllUsers= async (req, res, next)=>{
+    try {
+        const users= await User.find();
+
+        return res.status(200).json({message:"OK", users});
+    } catch (error) {
+        return res.status(200).json({message:"ERROR", cause:error.message });
+    }
+}
+export const userSignup= async (req, res, next)=>{
+    try {
+        const users= await User.find();
+
+        return res.status(200).json({message:" SIGNUP OK", users});
+    } catch (error) {
+        return res.status(200).json({message:"ERROR", cause:error.message });
+    }
 }
