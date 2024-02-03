566f47c927c1026c2eea1e6977a5ebedcfbc7c7c
unknown <ayushcodingprofile@gmail.com>
2024-02-03 18:37:14 +0530

    Login Form Frontend

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
-import { Box , Typography} from '@mui/material'
+import { Box , Typography, Button} from '@mui/material'
+import CustomizedInput from '../components/shared/CustomizedInput'
+import { FiLogIn } from "react-icons/fi";
 
 const Login = () => {
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email =  formData.get("email");
+    const password =  formData.get("password");
+    console.log(email, password);
+        
+
+  };
+
   return <Box width={"100%"}
   height = {"100%"}
   display="flex"
@@ -11,7 +23,7 @@ const Login = () => {
       <img src="airobot.png" alt="Robot" style={{width:"400px"}} />
     </Box>
     <Box display={'flex'} flex={{xs:1, md:0.5}} justifyContent={'center'} alignItems={'center'} padding={2} ml={"auto"} mt={"16"}>
-      <form style={{margin:"auto", padding:"30px", boxShadow:"10px 10px 20px #000",
+      <form onSubmit={(handleSubmit)} style={{margin:"auto", padding:"30px", boxShadow:"10px 10px 20px #000",
     borderRadius:"10px", border:"none"}}>
         <Box sx={{
           display:"flex",
@@ -19,6 +31,19 @@ const Login = () => {
           justifyContent:"center",
         }}>
             <Typography variant='h4' textAlign={"center"} padding={2} fontWeight={600}> Login</Typography>
+            <CustomizedInput type='email' name='email' label='Email'/>
+            <CustomizedInput type='password' name='password' label='Password'/>
+            <Button type='submit' sx={{px:2, py:1, mt:2, width:"400px", borderRadius:2, bgcolor:"#00fffc",
+            ":hover":{
+              bgcolor:"white",
+              color:"black",
+            },
+          }}
+            endIcon={<FiLogIn />
+}
+            >
+              Login
+            </Button>
         </Box>
       </form>
     </Box>
